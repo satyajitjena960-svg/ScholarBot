4bb04ff1021e25207a2657d0756c0e92b80aaf7d
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-06-18 07:50:46 +0530

    chore: initial commit with fresh modular architecture setup

---
+++ scholarbot-backend/src/test/java/com/scholarbot/ScholarbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholarbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
