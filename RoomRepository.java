fd65306aaa69ca3c2ec2f545cf87ab2ef96ad4b3
Debapriya0016 <omm217811@gmail.com>
2026-06-28 20:28:54 +0530

    Delete RoomRepository.java

--- RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.scholarbot.backend.repository;
-
-import com.scholarbot.backend.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, String> {
-}
